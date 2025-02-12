a653c7f7a72c40b95f36b748b63058302910fdd7
almeida2i <isaque.aulas@gmail.com>
2025-02-11 23:44:14 -0300

    Primeiro commit

---
+++ tarefaServomotor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
+
+#define SERVO_PIN 12
+
+void incre_advanc(void)
+{
+    uint16_t level = 1638;
+
+    while(level < 7864){
+        pwm_set_gpio_level(SERVO_PIN,level);
+        level += 65535u/4000;
+        sleep_ms(10);
+    }
+
+    level = 7864;
+
+    while(level > 1638){
+        pwm_set_gpio_level(SERVO_PIN,level);
+        level -= 65535u/4000;
+        sleep_ms(10);
+    }
+}
+
+int main()
+{
+    // Inicialização da comunicação serial
+    stdio_init_all();
+
+    gpio_set_function(SERVO_PIN,GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
+    pwm_set_clkdiv(slice_num,38.14697);
+    pwm_set_gpio_level(SERVO_PIN,0);
+    pwm_set_enabled(slice_num,true);
+
+    while (1) {
+        pwm_set_gpio_level(SERVO_PIN,7864); // 12%
+        sleep_ms(5000);
+        pwm_set_gpio_level(SERVO_PIN,4817); // 7,35%
+        sleep_ms(5000);
+        pwm_set_gpio_level(SERVO_PIN,1638); // 2,5%
+        sleep_ms(5000);
+
+        while(1)
+            incre_advanc();
+    }
+    return 0;
+}
